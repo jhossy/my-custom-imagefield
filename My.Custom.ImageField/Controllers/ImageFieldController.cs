10ec40960c074a8baacc4f3c1a4c07b9afb2e603
Jesper Hossy <jhossy@hotmail.com>
2019-09-22 09:29:57 +0200

    refactored ImageFieldController

--- My.Custom.ImageField/Controllers/ImageFieldController.cs
+++ My.Custom.ImageField/Controllers/ImageFieldController.cs
@@ -1,6 +1,7 @@
 ﻿using My.Custom.ImageField.Infrastructure.Search;
 using My.Custom.ImageField.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -10,6 +11,9 @@ namespace My.Custom.ImageField.Controllers
     {
         private readonly ImageAssetFactory _factory;
         private readonly ImageSearchService _searchService;
+
+        private readonly int _startPage = 0;
+        private readonly int _pageSize = 100;
 
         public ImageFieldController()
         {
@@ -21,22 +25,25 @@ namespace My.Custom.ImageField.Controllers
         {
             var viewModel = new ImageAssetViewModel();
 
-            var result = _searchService.Search(string.Empty, 0, 100);
-
-            viewModel.Images = result.Select(searchResult => new ImageAsset(){Id = Guid.Parse(searchResult.Id), Name = searchResult.Name, ImageUrl = searchResult.Url}).ToList();
+            viewModel.Images = ExecuteSearch(string.Empty, _startPage, _pageSize);
 
             return View("~/Views/Search/Index.cshtml", viewModel);
         }
 
         public ActionResult DoSearch(string query = "")
         {
-            var viewModel = new ImageAssetViewModel();
+            List<ImageAsset> searchResult = ExecuteSearch(query, _startPage, _pageSize);
 
-            var result = _searchService.Search(query, 0, 100);
-
-            var searchResultViewModel = result.Select(searchResult => new ImageAsset() { Id = Guid.Parse(searchResult.Id), Name = searchResult.Name, ImageUrl = searchResult.Url }).ToList();
-
-            return View("~/Views/Search/AjaxSearchResult.cshtml", searchResultViewModel);
+            return View("~/Views/Search/AjaxSearchResult.cshtml", searchResult);
         }        
+
+        private List<ImageAsset> ExecuteSearch(string query, int currentPage, int pageSize)
+        {
+            var result = _searchService.Search(query, currentPage, pageSize);
+
+            return result
+                .Select(searchResult => new ImageAsset() { Id = Guid.Parse(searchResult.Id), Name = searchResult.Name, ImageUrl = searchResult.Url })
+                .ToList();
+        }
     }
 }
